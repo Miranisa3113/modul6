9ab5a91731fda31477d6ad41a927148d49824bf2
Miranisa3113 <miratunn91@gmail.com>
2026-04-05 15:42:20 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul6_103082400001/Program.cs
+++ modul6_103082400001/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        SayaTubeUser user = new SayaTubeUser("Mira");
+
+        string[] film = {
+            "Venom", "Minions", "Interstellar", "Titanic",
+            "Frozen", "Joker", "Spiderman", "Batman",
+            "Harry Potter", "Parasite"
+        };
+
+        foreach (var f in film)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Review Film " + f + " oleh Mira");
+            video.IncreasePlayCount(100);
+            user.AddVideo(video);
+        }
+
+        user.PrintAllVideoPlaycount();
+        Console.WriteLine("Total Play Count: " + user.GetTotalVideoPlayCount());
+    }
+}
---
+++ modul6_103082400001/SayaTubeUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SayaTubeUser
+{
+    private int id;
+    private List<SayaTubeVideo> uploadedVideos;
+    public string Username;
+
+    public SayaTubeUser(string username)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.Username = username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public void AddVideo(SayaTubeVideo video)
+    {
+        uploadedVideos.Add(video);
+    }
+
+    public int GetTotalVideoPlayCount()
+    {
+        int total = 0;
+        foreach (var video in uploadedVideos)
+        {
+            total += video.GetPlayCount();
+        }
+        return total;
+    }
+
+    public void PrintAllVideoPlaycount()
+    {
+        Console.WriteLine("User: " + Username);
+
+        int i = 1;
+        foreach (var video in uploadedVideos)
+        {
+            Console.WriteLine("Video " + i + " judul: " + video.GetTitle());
+            i++;
+        }
+    }
+}
